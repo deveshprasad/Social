846986bf3e7bb897ae7776a57be6cfc2374a789d
Devesh Prasad <63739986+deveshprasad@users.noreply.github.com>
2021-03-30 20:24:34 +0530

    Updated  Comment.js

--- routes/comments.js
+++ routes/comments.js
@@ -6,14 +6,19 @@ const middleware=require("../middleware");
 ////////////////////////////////////////////////////// View Comment
 router.get("/new",middleware.isLoggedIn,(req,res)=>{
     social.findById(req.params.id,(err,social)=>{
-        if(err){console.log(err);}
-        else{res.render("comments/new",{social:social});}})
+        if(err){
+            console.log(err);
+               }
+        else{
+            res.render("comments/new",{social:social});
+        }
+    })
 });
 /////////////////////////////////////////////////////// Adding Comment
 router.post("/",middleware.isLoggedIn,(req,res)=>{
-social.findById(req.params.id,(err,social)=>{
-  if(err){req.flash("error","Something Went Wrong ! Please Refresh Or Try Again ");
-    console.log(error);}
+    social.findById(req.params.id,(err,social)=>{
+    if(err){req.flash("error","Something Went Wrong ! Please Refresh Or Try Again ");
+        console.log(error);}
     else{Comment.create(req.body.comment,(err,comment)=>{
         if(err){console.log(err);}
         else{
@@ -24,29 +29,50 @@ social.findById(req.params.id,(err,social)=>{
             social.save();
             console.log(comment);
             req.flash("success","Successfully Added Comment! Please Refresh If Not Visible!");
-            res.redirect("/socials/"+social._id);}});}});
+            res.redirect("/socials/"+social._id);
+        }
+    });
+  }});
 });
 ///////////////////////////////////////////////////////////////////////////// Editing Comment
 router.get("/:comment_id/edit",middleware.checkCommentOwnership,(req,res)=>{
     social.findById(req.params.id,(err,foundsocial)=>{
-        if(err||!foundsocial){req.flash("error","Sorry! Cannot Find It ! Please Delete It And Add A New One");
+        if(err||!foundsocial){
+            req.flash("error","Sorry! Cannot Find It ! Please Delete It And Add A New One");
             console.log(err);
-            return res.redirect("back");}
+            return res.redirect("back");
+                             }
         Comment.findById(req.params.comment_id,(err,foundComment)=>{
-            if(err){res.redirect("back");}
-            else{res.render("comments/edit",{social_id:req.params.id,comment:foundComment});}});});
+            if(err){
+                res.redirect("back");
+            }
+            else{
+                res.render("comments/edit",{social_id:req.params.id,comment:foundComment});
+            }
+        });
+    });
 });
 ///////////////////////////////////////////////////////////////////////////  Updating Comment
 router.put("/:comment_id",middleware.checkCommentOwnership,(req,res)=>{
 Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,(err)=>{
-if(err){res.redirect("back");}
-else{res.redirect("/socials/"+req.params.id);}});
+if(err){
+    res.redirect("back");
+}
+else{
+    res.redirect("/socials/"+req.params.id);
+}
+});
 });
 ////////////////////////////////////////////////////////////////////////////// Delete Comment
 router.delete("/:comment_id",middleware.checkCommentOwnership ,(req,res)=>{
     Comment.findByIdAndDelete(req.params.comment_id,(err,deleted)=>{
-        if(err){res.redirect("back");}
-        else{req.flash("success","Comment Deleted ! PLease Refresh If Not Visible");
-             res.redirect("/socials/"+req.params.id);}});
+        if(err){
+            res.redirect("back");
+        }
+        else{
+            req.flash("success","Comment Deleted ! PLease Refresh If Not Visible");
+             res.redirect("/socials/"+req.params.id);
+        }
+    });
 });
 module.exports=router;
