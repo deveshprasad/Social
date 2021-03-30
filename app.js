0141f59f30bb9f6aa75444e7242a776baa1249a4
Devesh Prasad <63739986+deveshprasad@users.noreply.github.com>
2021-03-30 20:12:10 +0530

    Updated App.js

--- app.js
+++ app.js
@@ -18,29 +18,45 @@ const commentRoutes=require("./routes/comments"),
 socialRoutes=require("./routes/socials"),
 indexRoutes=require("./routes/index");
 const app=express();
+
 mongoose.connect(process.env.databaseURl,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false,useCreateIndex:true});
+
 app.use(express.json());
+
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.set("view engine", "ejs");
+
 app.use(express.static(__dirname+"/public"));
+
 app.use(methodOverride("_method"));
+
 app.use(flash());
+
 app.use(expressSanitizer());
+
 app.locals.moment = require('moment');
+
 app.use(require("express-session")({
     secret:process.env.secretMessage,
     resave:false,
     saveUninitialized:false
 }));
+
 app.use(passport.initialize());
+
 app.use(passport.session());
+
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(function(user, done){done(null, user.id);});
+
 passport.deserializeUser(function(id, done){
     User.findById(id, function(err, user) {
       done(err, user);
     });
 });
+
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -53,15 +69,20 @@ passport.use(new GoogleStrategy({
     });
   }
 ));
+
 app.use(function(req,res,next){
 res.locals.currentUser=req.user;
 res.locals.error=req.flash("error");
 res.locals.success=req.flash("success");
 next();
 });
+
 app.use('/',indexRoutes);
+
 app.use("/socials",socialRoutes);
+
 app.use("/socials/:id/comments",commentRoutes);
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
