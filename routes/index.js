6e0f4b68cb4b0159d586d395b81586610b7421f9
Devesh Prasad <63739986+deveshprasad@users.noreply.github.com>
2021-03-30 20:36:00 +0530

    Update  index.js

--- routes/index.js
+++ routes/index.js
@@ -13,27 +13,42 @@ var { promisify } = require('util');
 var sizeOf = promisify(require('image-size'));
 var multer = require('multer');
 var storage = multer.diskStorage({filename: function(req, file, callback) {callback(null, Date.now() + file.originalname);}});
+
 var imageFilter = function(req, file, cb) {
   if (!file.originalname.match(/\.(jpg|jpeg|png)$/i)) {
     return cb(new Error('Only image files are allowed!'), false);}
   cb(null, true);};
+
 var upload = multer({ storage: storage, fileFilter: imageFilter })
 var cloudinary = require('cloudinary');
+
 cloudinary.config({
   cloud_name: 'dcsgaregd',
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
 /////////////////////////////////////////////// Landing Page
 router.get("/",(req,res)=>{res.render("landing");});
+
 //////////////////////////////////////////////////////////// Documentation Page
-router.get("/documentation",middleware.isLoggedIn,(req,res)=>{res.render("documentation");});
+router.get("/documentation",middleware.isLoggedIn,(req,res)=>{
+  res.render("documentation");
+});
+
 /////////////////////////////////////////////////////////////////////////////////////Google Authentication
 router.get("/auth/google",passport.authenticate("google",{scope:["email"]}));
+
 router.get('/auth/google/secrets', passport.authenticate('google',{ failureRedirect: '/login' }),
-function(req, res) {res.redirect('/socials');});
+function(req, res) {
+  res.redirect('/socials');
+});
+
 //////////////////////////////////////////////////////////////////// Sign Up Page
-router.get("/register",(req,res)=>{res.render("register");});
+router.get("/register",(req,res)=>{
+  res.render("register");
+});
+
 router.post("/register", upload.single('avatar'), function(req, res) {
 cloudinary.v2.uploader.upload(req.file.path,function(err,result){
   if(err) {
@@ -50,30 +65,46 @@ cloudinary.v2.uploader.upload(req.file.path,function(err,result){
         avatarId: result.public_id,
         description: req.body.description
       })
-      if(req.body.adminCode===process.env.adminCode){newUser.isAdmin=true;}
+      if(req.body.adminCode===process.env.adminCode){
+        newUser.isAdmin=true;
+      }
       User.register(newUser, req.body.password, function(err, user) {
-        if (err) {return res.render("register", { "error": err.message + " Email Already Exists !" });}
+        if (err) {
+          return res.render("register", { "error": err.message + " Email Already Exists !" })
+          ;}
         else {passport.authenticate("local")(req, res, function() {
               console.log(req.body.username);
               req.flash("success", "Welcome to Socials ! " + user.firstName);
               return res.redirect("/socials");})}})
 });
 })
+
 ///////////////////////////////////////////////////// Login Page
-router.get("/login",(req,res)=>{res.render("login");});
-router.post("/login",passport.authenticate("local",{successRedirect:"/socials",failureRedirect:"/login"}),(req,res)=>{});
+router.get("/login",(req,res)=>{
+  res.render("login");
+});
+
+router.post("/login",passport.authenticate("local",
+                                           {successRedirect:"/socials",failureRedirect:"/login"}),(req,res)=>{});
+
 /////////////////////////////////////////////////////////  Logout Page
 router.get("/logout",(req,res)=>{req.logout();
  req.flash("success","Successfly, Logged You Out! Please Visit Us Again");
- res.redirect("/socials");});
+ res.redirect("/socials");
+ });
+
 //////////////////////////////////////////////////////   Forgot Page
-router.get('/forgot', function(req, res){res.render('forgot');});
+router.get('/forgot', function(req, res){
+  res.render('forgot');
+});
+
 router.post('/forgot', function(req, res, next) {
     async.waterfall([
       function(done) {
         crypto.randomBytes(20, function(err, buf) {
           var token = buf.toString('hex');
-          done(err, token);});
+          done(err, token);
+        });
       },
       function(token, done) {
         User.findOne({ email: req.body.email }, function(err, user) {
@@ -84,13 +115,15 @@ router.post('/forgot', function(req, res, next) {
           user.resetPasswordToken = token;
           user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
           user.save(function(err) {
-            done(err, token, user);});
+            done(err, token, user);
+          });
         });
       },
       function(token, user, done) {
         var smtpTransport = nodemailer.createTransport({
           service: 'Gmail', 
-          auth: {user: 'ddddevesh2001@gmail.com',pass: process.env.gmailPassword}});
+          auth: {user: 'ddddevesh2001@gmail.com',pass: process.env.gmailPassword}
+        });
         var mailOptions = {
           to: user.email,
           from: 'ddddevesh2001@gmail.com',
@@ -103,30 +136,44 @@ router.post('/forgot', function(req, res, next) {
         smtpTransport.sendMail(mailOptions, function(err) {
           console.log('mail sent');
           req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions. Please ! Check your spam folder. You have 60 mins.');
-          done(err, 'done');});}], function(err) {if (err) return next(err);res.redirect('/forgot');});
+          done(err, 'done');
+        });
+      }], function(err) {if (err) return next(err);
+                         res.redirect('/forgot');
+      });
 });
+
 ////////////////////////////////////////////////  Reset Page  
 router.get('/reset/:token', function(req, res) {
     User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-      if (!user) {req.flash('error', 'Password reset token is invalid or has expired.');
-                  return res.redirect('/forgot');}
-      res.render('reset', {token: req.params.token});});
+      if (!user) {
+        req.flash('error', 'Password reset token is invalid or has expired.');
+                  return res.redirect('/forgot');
+      }
+      res.render('reset', {token: req.params.token});
+    });
 });
+
 router.post('/reset/:token', function(req, res) {
     async.waterfall([
       function(done) {
         User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
           if (!user) {req.flash('error', 'Password reset token is invalid or has expired.');
-            return res.redirect('back');}
+            return res.redirect('back');
+                     }
           if(req.body.password === req.body.confirm) {
             user.setPassword(req.body.password, function(err) {
               user.resetPasswordToken = undefined;
               user.resetPasswordExpires = undefined;
               user.save(function(err) {
-                req.logIn(user, function(err) {done(err, user);});
+                req.logIn(user, function(err) {done(err, user);
+                });
               });
             })}else{req.flash("error", "Passwords do not match. Please! Try Again.");
-                    return res.redirect('back');}});},
+                    return res.redirect('back');
+                   }
+        });
+      },
       function(user, done) {
         var smtpTransport = nodemailer.createTransport({
           service: 'Gmail', 
@@ -151,19 +198,28 @@ router.post('/reset/:token', function(req, res) {
       res.redirect('/socials');
     });
   });
+
  ///////////////////////////////////////////////// User Profile Page
  router.get("/users/:id",(req,res)=>{
   User.findById(req.params.id,(err,foundUser)=>{
-      if(err){req.flash("error","Something Went Wrong")
-              res.redirect("/");}
+      if(err){
+              req.flash("error","Something Went Wrong")
+              res.redirect("/");
+             }
       Social.find().where("author.id").equals(foundUser._id).exec((err,socials)=>{
-        if(err){req.flash("error","Something Went Wrong")
-                res.redirect("/");}   
+        if(err){
+                req.flash("error","Something Went Wrong")
+                res.redirect("/");
+        }   
         res.render("users/show",{user:foundUser,socials:socials})
       });});
  });  
- ///////////////////////////////////////////////////// Contact Page
-router.get("/contact",middleware.isLoggedIn,(req,res)=>{res.render("contact");});
+ 
+///////////////////////////////////////////////////// Contact Page
+router.get("/contact",middleware.isLoggedIn,(req,res)=>{
+  res.render("contact");
+});
+
 router.post('/contact',middleware.isLoggedIn,async (req, res) => {
   let { name, email, message } = req.body;
   name = req.sanitize(name);
@@ -185,7 +241,9 @@ router.post('/contact',middleware.isLoggedIn,async (req, res) => {
     res.redirect('/contact');
   } catch (error) {
     console.error(error);
-    if (error.response) {console.error(error.response.body)}
+    if (error.response) {
+      console.error(error.response.body)
+    }
     req.flash('error', 'Sorry, something went wrong, please contact deveshprasad577@yahoo.com');
     res.redirect('back');
   }
