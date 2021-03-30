7e7cd5159d26abe8aca44e5bac9bee04f4e1002f
Devesh Prasad <63739986+deveshprasad@users.noreply.github.com>
2021-03-30 20:14:55 +0530

    Updated index.js

--- middleware/index.js
+++ middleware/index.js
@@ -1,17 +1,30 @@
 const social=require("../models/social");
 const Comment=require("../models/comment");
+
 var middlewareObj={};
+
 middlewareObj.checksocialOwnership=function(req,res,next){
     if(req.isAuthenticated()){
         social.findById(req.params.id,(err,foundsocial)=>{
             if(err||!foundsocial){
                 req.flash("error","social not found");
-                res.redirect("back");}
-                else{if(foundsocial.author.id.equals(req.user._id)||req.user.isAdmin){next();}
+                res.redirect("back");
+            }
+                else{
+                    if(foundsocial.author.id.equals(req.user._id)||req.user.isAdmin){
+                        next();
+                    }
                     else{req.flash("error","Sorry You dont have permijssion to do that ");
-                        res.redirect("back");}}});}
+                        res.redirect("back");
+                        }
+                }
+        });
+    }
             else{req.flash("error","You need to be logged in to do that")
-                 res.redirect("back");}}
+                 res.redirect("back");
+                }
+}
+
 middlewareObj.checkCommentOwnership=function(req,res,next){
     if(req.isAuthenticated()){
         Comment.findById(req.params.comment_id,(err,foundComment)=>{
@@ -25,9 +38,16 @@ middlewareObj.checkCommentOwnership=function(req,res,next){
                     req.flash("error","Come on you dont have permission!");
                     res.redirect("back");}}});}
                 else{req.flash("error","Please Login First !");
-                    res.redirect("back");}}
+                    res.redirect("back");
+                    }
+}
+
 middlewareObj.isLoggedIn=function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){return next();}
+    if(req.isAuthenticated()){
+        return next();
+    }
     req.flash("error","Please login first !");
-    res.redirect("/login");}
+    res.redirect("/login");
+}
+
 module.exports=middlewareObj;
